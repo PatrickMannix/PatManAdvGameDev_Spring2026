85a08afed4f2039711d5a606f8b66624d7b84992
PatrickMannix <94652969+PatrickMannix@users.noreply.github.com>
2026-02-04 17:32:22 -0500

    init

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    private float jumpSpeed;
+    private float xMove;
+    private float xVelocity;
+
+    private Rigidbody2D rb;
+
+    private bool jumpFlag = false;
+
+    public LayerMask ground;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMove = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            jumpFlag = true;
+        }
+
+        //Debug.Log(IsGrounded());
+        //transform.Translate(xMove * movementSpeed * Time.deltaTime, 0, 0);
+    }
+    private void FixedUpdate()
+    {
+        float xVelocity = xMove * movementSpeed * Time.deltaTime;
+        rb.linearVelocity = new Vector3(xVelocity, rb.linearVelocity.y, 0);
+
+        if(jumpFlag)
+        {
+            rb.linearVelocityY = jumpSpeed;
+            jumpFlag = false;
+        }
+    }
+    private bool IsGrounded()
+    {
+        float radius = GetComponent<Collider2D>().bounds.extents.x;
+        float dist = GetComponent<Collider2D>().bounds.extents.y;
+
+        return Physics2D.CircleCast(transform.position, radius, Vector2.down, dist);
+    }
+}
